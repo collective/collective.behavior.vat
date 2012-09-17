7e68f3d468d8ce88585ba6bec5d4915687e0a1b7
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-09-17 23:20:00 +0300

    Release 0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='collective.behavior.vat',
-    version='0.1',
+    version='0.2',
     description="Adds VAT field to dexterity content type.",
     long_description=long_description,
     # Get more strings from
