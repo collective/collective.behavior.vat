8ac71d86029362deaba2ec4dc8aab2d305230a62
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-10-18 10:20:39 +0300

    Updated change log.

--- collective/behavior/vat/tests/dexterity/folder.py
+++ collective/behavior/vat/tests/dexterity/folder.py
@@ -4,11 +4,3 @@ from plone.directives import form
 
 class ITestFolder(form.Schema):
     """Schema interface for TestFolder"""
-
-
-# class View(grok.View):
-#     """Default view for page."""
-
-#     grok.context(ITestFolder)
-#     grok.require('zope2.View')
-#     grok.name('view')
